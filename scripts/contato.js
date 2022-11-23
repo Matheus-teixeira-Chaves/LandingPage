57b683257c4cfd868ae3994e343eb438b7e57853
Matheus Frazão <73357559+Matheus-teixeira-Chaves@users.noreply.github.com>
2022-11-23 00:02:34 -0300

    1) Melhorias no botao de darkMode, 2) Contatos - Parte 1

---
+++ scripts/contato.js
@@ -0,0 +1,15 @@
+function alterarImagemLinkedIn () {
+    document.getElementById('logoLinkedIn').src = "imagens/Logo-LinkedIn-roxo-50x50.png";
+}
+
+function retornarImagemLinkedIn () {
+    document.getElementById('logoLinkedIn').src = "imagens/Logo-LinkedIn-branca-50x50.png";
+}
+
+function alterarImagemGithub () {
+    document.getElementById("logoGithub").src = "imagens/github_roxo_50x50.png";
+}
+
+function retornarImagemGithub () {
+    document.getElementById("logoGithub").src = "imagens/github_branco_50x50.png";
+}
