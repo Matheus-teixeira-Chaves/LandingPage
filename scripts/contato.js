c1f50f486ee820e81e8f67684b1ca8ea23eba60f
Matheus Frazão <73357559+Matheus-teixeira-Chaves@users.noreply.github.com>
2022-11-23 20:35:27 -0300

    Contatos - Parte 2

--- scripts/contato.js
+++ scripts/contato.js
@@ -1,15 +1,31 @@
 function alterarImagemLinkedIn () {
-    document.getElementById('logoLinkedIn').src = "imagens/Logo-LinkedIn-roxo-50x50.png";
+    document.getElementById('logoLinkedIn').src = "imagens/imagens_footer/Logo-LinkedIn-roxo-50x50.png";
 }
 
 function retornarImagemLinkedIn () {
-    document.getElementById('logoLinkedIn').src = "imagens/Logo-LinkedIn-branca-50x50.png";
+    document.getElementById('logoLinkedIn').src = "imagens/imagens_footer/Logo-LinkedIn-branca-50x50.png";
 }
 
 function alterarImagemGithub () {
-    document.getElementById("logoGithub").src = "imagens/github_roxo_50x50.png";
+    document.getElementById("logoGithub").src = "imagens/imagens_footer/github_roxo_50x50.png";
 }
 
 function retornarImagemGithub () {
-    document.getElementById("logoGithub").src = "imagens/github_branco_50x50.png";
+    document.getElementById("logoGithub").src = "imagens/imagens_footer/github_branco_50x50.png";
+}
+
+function alterarImagemEmail () {
+    document.getElementById("logoEmail").src = "imagens/imagens_footer/email_roxo_50x50.png";
+}
+
+function retornarImagemEmail () {
+    document.getElementById("logoEmail").src = "imagens/imagens_footer/email_branco_50x50.png";
+}
+
+function alterarImagemWhats () {
+    document.getElementById("logoWhats").src = "imagens/imagens_footer/whatsapp_fundo_transpar_contorno_roxo_50x50.png";
+}
+
+function retornarImagemWhats () {
+    document.getElementById("logoWhats").src = "imagens/imagens_footer/whatsapp_fundo_transpar_contorno_brancoente_50x50.png";
 }
